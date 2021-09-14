b387c79aa88c20e9bed852657b34e52504eb5297
PsypherPunk <psypherpunk@gmail.com>
2021-09-14 17:48:37 +0100

    docs: include email block in examples

--- examples/resources/ctfd_setup_resource/resource.tf
+++ examples/resources/ctfd_setup_resource/resource.tf
@@ -3,5 +3,16 @@ resource "ctfd_setup" "setup" {
   description        = "Example CTFd setup."
   admin_email        = "admin@example.com"
   configuration_path = "/tmp/juice-shop-ctf.zip"
+
+  email {
+    username     = "admin@example.com"
+    password     = "secure-password"
+    from_address = "admin+ctfd@example.com"
+    server       = "smtp.example.com"
+    port         = 587
+    use_auth     = true
+    use_tls      = true
+    use_ssl      = false
+  }
 }
 
